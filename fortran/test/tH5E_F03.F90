aa1ef8527873d799759a02a8d8d25dc83e472f83
Scot Breitenfeld <brtnfld@hdfgroup.org>
2024-03-14 17:18:42 -0500

    debug3

--- fortran/test/tH5E_F03.F90
+++ fortran/test/tH5E_F03.F90
@@ -300,7 +300,7 @@ SUBROUTINE test_error_stack(total_error)
 !       arg1=ACHAR(27)//"[31m", arg2=ACHAR(27)//"[0m" )
   CALL H5Epush_f(estack_id, cls_id, major, minor, "%s ERROR TEXT %s"//achar(10), error, &
        ptr1, ptr2, ptr3, &
-       arg1=ACHAR(27)//"[31m", arg2=ACHAR(27)//"[0m" ))
+       arg1=ACHAR(27)//"[31m", arg2=ACHAR(27)//"[0m" )
   CALL check("H5Epush_f", error, total_error)
 
   CALL h5eget_num_f(estack_id, count, error)
