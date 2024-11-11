c87872c577fcda6cda9836c9f875d31238a34b0d
Scot Breitenfeld <brtnfld@hdfgroup.org>
2024-11-11 15:28:30 -0600

    test

--- fortran/test/tH5R.F90
+++ fortran/test/tH5R.F90
@@ -684,7 +684,7 @@ SUBROUTINE v3reftest(cleanup, total_error)
   PRINT*,"Casdfdsf4"
   CALL h5rget_obj_name_f(C_LOC(ref_ptr(3)), buf_big, error)
   PRINT*,"Casdfdsf3"
-  CALL h5rget_obj_name_f(C_LOC(ref_ptr(2)), buf_big, error
+  CALL h5rget_obj_name_f(C_LOC(ref_ptr(2)), buf_big, error)
   PRINT*,"Casdfdsf2"
   CALL h5rget_obj_name_f(C_LOC(ref_ptr(1)), buf_big, error)
   PRINT*,"Csdfdsfsd"
