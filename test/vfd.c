de5c455eef31f511b87c8226cc6c4d943138c7a7
Scot Breitenfeld <brtnfld@hdfgroup.org>
2026-06-27 23:14:14 -0500

    Fix test_log() flag check: revert incorrect H5FD_FEAT_SUPPORTS_VFD_SWMR
    H5FDlog.c only sets H5FD_FEAT_SUPPORTS_SWMR_IO, not
    H5FD_FEAT_SUPPORTS_VFD_SWMR. The previous commit incorrectly added the
    VFD_SWMR flag to the test_log() exact-match check; remove it.

--- test/vfd.c
+++ test/vfd.c
@@ -1945,8 +1945,7 @@ test_log(void)
     /* Check for extra flags not accounted for above */
     if (driver_flags != (H5FD_FEAT_AGGREGATE_METADATA | H5FD_FEAT_ACCUMULATE_METADATA | H5FD_FEAT_DATA_SIEVE |
                          H5FD_FEAT_AGGREGATE_SMALLDATA | H5FD_FEAT_POSIX_COMPAT_HANDLE |
-                         H5FD_FEAT_SUPPORTS_SWMR_IO | H5FD_FEAT_DEFAULT_VFD_COMPATIBLE |
-                         H5FD_FEAT_SUPPORTS_VFD_SWMR))
+                         H5FD_FEAT_SUPPORTS_SWMR_IO | H5FD_FEAT_DEFAULT_VFD_COMPATIBLE))
         TEST_ERROR;
 
     /* Create the test file */
